e6d0e9f1426fa93d2cedea1aa271b7c63466b596
rezky08 <rezky.setiawan85@gmail.com>
2021-07-11 12:55:37 +0700

    [LOGGER] - add ip and url logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,13 @@
 const moment = require("moment");
+const geoip = require("geoip-lite");
+
 const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get("host")}: ${moment().format()}`);
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(
+    `${ip}\t${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }\t${moment().format()}\t${geoip.lookup(ip)}`
+  );
   next();
 };
 
