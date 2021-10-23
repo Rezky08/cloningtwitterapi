95a2193e218672b628103f18ae06ce20efe66ccb
rezky08 <rezky.setiawan85@gmail.com>
2021-10-23 14:08:11 +0700

    - move aggregation to query - make search case insensitive

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,50 +2,12 @@ const Response = require("../responses");
 const Tweet = require("../models/Tweet");
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const UserQuery = require("../queries/User");
 const { errorResponse } = require("../responses/responseFormatter");
 const ObjectId = mongoose.Types.ObjectId;
 
 const index = (req, res) => {
-  const q = req.query?.q;
-
-  User.aggregate([
-    {
-      $match: { username: { $regex: `.*${q}.*` } },
-    },
-    {
-      $lookup: {
-        from: "userdetails",
-        localField: "_id",
-        foreignField: "user",
-        as: "detail",
-      },
-    },
-    {
-      $lookup: {
-        from: "follows",
-        localField: "_id",
-        foreignField: "user",
-        as: "follows",
-      },
-    },
-    {
-      $unwind: "$detail",
-    },
-    {
-      $unwind: "$follows",
-    },
-    {
-      $project: {
-        username: 1,
-        description: "$detail.description",
-        location: "$detail.location",
-        link: "$detail.link",
-        followers: { $size: "$follows.followers" },
-        following: { $size: "$follows.following" },
-      },
-    },
-    { $limit: 5 },
-  ])
+  User.aggregate(UserQuery.searchAggregate(req))
     .then((user) => {
       Response.ResponseFormatter.jsonResponse(
         res,
--- queries/User.js
+++ queries/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const PaginationQuery = require("./Pagination");
 
 const userAggregate = (req) => [
   {
@@ -61,6 +62,46 @@ const userAggregate = (req) => [
   // { $limit: 1 },
 ];
 
+const searchAggregate = (req) => [
+  {
+    $match: { username: { $regex: `.*${req.query.q}.*`, $options: "i" } },
+  },
+  {
+    $lookup: {
+      from: "userdetails",
+      localField: "_id",
+      foreignField: "user",
+      as: "detail",
+    },
+  },
+  {
+    $lookup: {
+      from: "follows",
+      localField: "_id",
+      foreignField: "user",
+      as: "follows",
+    },
+  },
+  {
+    $unwind: "$detail",
+  },
+  {
+    $unwind: "$follows",
+  },
+  {
+    $project: {
+      username: 1,
+      description: "$detail.description",
+      location: "$detail.location",
+      link: "$detail.link",
+      followers: { $size: "$follows.followers" },
+      following: { $size: "$follows.following" },
+    },
+  },
+  ...PaginationQuery.pagination(req?.query?.page, req?.query?.perpage),
+];
+
 module.exports = {
   userAggregate,
+  searchAggregate,
 };
